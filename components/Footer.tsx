e337902e382b5c78666012772616d091f5d8c940
secretariadistrito4320-max <secretariadistrito4320@gmail.com>
2026-08-19 02:01:15 -0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -61,15 +61,17 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10 sm:py-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
           
-          {/* Column 1: Identity & Official Logo */}
+          {/* Column 1: Identity & Official Logo Agrandado */}
           <div className="lg:col-span-2 space-y-4">
             <Link href="/" className="inline-block">
-              <div className="relative w-72 h-24 sm:w-80 sm:h-28">
+              {/* Tamaño aumentado a 72/80 de ancho y 20/24 de alto */}
+              <div className="relative w-72 h-20 sm:w-80 sm:h-24">
                 <Image
                   src="/images/logo-rotary_2.png"
                   alt="Rotary Distrito 4320"
                   fill
-                  sizes="256px"
+                  priority
+                  sizes="320px"
                   className="object-contain object-left"
                 />
               </div>
